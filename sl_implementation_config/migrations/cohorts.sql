6ba4c7e61a8ab0433da6c1f0f2643e048f4c7b42
root <root@localhost.localdomain>
2019-12-02 20:10:07 +0530

    Adding scheduled visit from mds server

--- sl_implementation_config/migrations/cohorts.sql
+++ sl_implementation_config/migrations/cohorts.sql
@@ -2,10 +2,10 @@ call add_concept(@concept_id, @concept_name_short_id, @concept_name_full_id, 'Co
 set @answerconcept_id26 = @concept_id;
 call add_concept(@concept_id, @concept_name_short_id, @concept_name_full_id, 'Cohort, non endTB', 'non endTB', 'N/A','Misc', FALSE);
 set @answerconcept_id27 = @concept_id;
-call add_concept(@concept_id, @concept_name_short_id, @concept_name_full_id, 'Belongs to external cohort', ' cohort', 'Coded','Misc', FALSE);
+call add_concept(@concept_id, @concept_name_short_id, @concept_name_full_id, 'Treatment cohort', ' cohort', 'Coded','Misc', FALSE);
 set @concept28_id = @concept_id;
 
 call add_concept_answer (@concept28_id,@answerconcept_id26,1);
 call add_concept_answer (@concept28_id,@answerconcept_id27,2);
 
-insert into program_attribute_type(name, description, datatype, datatype_config, min_occurs, creator, date_created, uuid) values('Belongs to external cohort','Cohort', 'org.bahmni.module.bahmnicore.customdatatype.datatype.CodedConceptDatatype', @concept28_id, 0, 1, NOW(), UUID());
+insert into program_attribute_type(name, description, datatype, datatype_config, min_occurs, creator, date_created, uuid) values('Treatment cohort','Cohort', 'org.bahmni.module.bahmnicore.customdatatype.datatype.CodedConceptDatatype', @concept28_id, 0, 1, NOW(), UUID());
